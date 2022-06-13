a0b9b6d5f635af0cd35aa136452f4e68b5ee0eea
jingdaicanada <59512036+jingdaicanada@users.noreply.github.com>
2022-06-13 07:21:45 -0700

    back to default template

--- Components/TrackPlayerService.js
+++
@@ -1,9 +0,0 @@
-import TrackPlayer, {Event, useTrackPlayerEvents} from 'react-native-track-player';
-
-module.exports = async function () {
-    // This service needs to be registered for the module to work
-    // but it will be used later in the "Receiving Events" section
-    useTrackPlayerEvents([Event.RemotePlay], () => TrackPlayer.play());
-    useTrackPlayerEvents([Event.RemotePaused], () => TrackPlayer.pause());
-    useTrackPlayerEvents([Event.RemoteStop], () => TrackPlayer.stop());
-};
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-import React from "react";
-import HomeScreen from './Screens/HomeScreen';
-export const applyCustomCode = externalCodeSetup => {
-    externalCodeSetup.navigationApi.addNavigationRoute(
-        "homePage",
-        "Home",
-        HomeScreen,
-        "Main" // "Auth" | "noAuth" | "Main" | "All"
-    );
+export const applyCustomCode = (externalCodeSetup: any) => {
+	// call custom code api here
 };
-
-
-
