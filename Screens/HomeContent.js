f6136798004cb7d3b3459476e56901a97c7a3075
jingdaicanada <59512036+jingdaicanada@users.noreply.github.com>
2022-06-21 09:27:49 -0700

    Trackplayer change remove upcoming tracks.

--- Screens/HomeContent.js
+++ Screens/HomeContent.js
@@ -34,7 +34,7 @@ const HomeContent = (props) => {
     const [quotesLoading, setQuotesLoading] = useState(true);
     TrackPlayer.updateOptions({
         stopWithApp: !(user&&user.membership&&user.membership.length), // false=> music continues in background even when app is closed
-        alwaysPauseOnInterruption: true,
+        alwaysPauseOnInterruption: false,
         // Media controls capabilities
         capabilities: [
             Capability.Play,
